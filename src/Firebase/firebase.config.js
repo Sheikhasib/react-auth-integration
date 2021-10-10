1446c3ae802061d81818e93aa13f847b353fdb35
Sheikhasib <sheikhhasib037@gmail.com>
2021-10-10 13:21:29 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNFTcEUvn_WVg5aH-7wVmhdHZeYFaltOQ",
+    authDomain: "simple-firebase-authenti-56305.firebaseapp.com",
+    projectId: "simple-firebase-authenti-56305",
+    storageBucket: "simple-firebase-authenti-56305.appspot.com",
+    messagingSenderId: "759182184995",
+    appId: "1:759182184995:web:3ca993313a14a60bfa5796"
+  };
+
+  export default firebaseConfig;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useAuth from '../../Hooks/useAuth';
+
+const Home = () => {
+    const {user} = useAuth();
+    return (
+        <div>
+            <h2>My Home</h2>
+            <h4>user: {user.displayName}</h4>
+        </div>
+    );
+};
+
+export default Home;
